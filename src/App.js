16e239f39eaa7a12ce70007081bba75b81037cd1
Jessica Classon <jessicaclasson4@gmail.com>
2021-12-29 14:42:18 -0500

    Add files via upload
    http://localhost:3000/

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {useFormik} from 'formik'
+// TODO: import useFormik from formik library
+
+function App() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+
+  onSubmit: values =>{
+    alert(" Successful login");
+  },
+
+
+  validate: values => {
+    let errors = {};
+    if(!values.username) errors.username = "Username should be an email";
+    if(!values.password) errors.password = "Field Required";
+    return errors;
+   }
+  });
+
+
+  // TODO: add a const called formik assigned to useFormik()
+  return (
+    <div>
+      < form onSubmit={formik.handleSubmit}>
+      <div>Username</div>
+      <input name="username" type="text" placeholder="Jess@gmail.com" onChange={formik.handleChange} value={formik.values.username}/>
+        {formik.errors.username ? <div style={{color:'red'}}>{formik.errors.username}</div>: null} 
+        <div>Password</div>
+        <input name="password" type="text" onChange={formik.handleChange} value={formik.values.password}/>
+        {formik.errors.password? <div style={{color:'red'}}>{formik.errors.password}</div>: null}
+        <div><br></br></div>
+        <div><input type="checkbox"/> Remember me</div>
+       <button type="submit">Submit</button>
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
